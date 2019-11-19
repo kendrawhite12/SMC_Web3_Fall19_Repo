be42a90b1148134c20de0272402cb316e4a8a240
kendrawhite12 <47585497+kendrawhite12@users.noreply.github.com>
2019-11-19 12:49:30 -0800

    index.html, canvas.js
    Expand of interactive animation HW
    I really struggled with this assignment.
    Need help making a counter to showing how many times the ball has been clicked.
    Needs control that would allow you to change ball color.

---
+++ Web HW 11:19/canvas.js
@@ -0,0 +1,68 @@
+
+var canvas = document.getElementById("Game");
+var ctx = canvas.getContext("2d");
+
+var ballX = canvas.width/2;
+var ballY = canvas.height/2;
+var ballColor = 'rgb(200, 135,247)';
+var ballRadius = 65;
+
+var speedX = 3;
+var speedY = 7;
+
+var directionDown = true;
+var directionRight = true;
+
+function animate(){
+    ctx.fillStyle = 'rgba(200, 255,247, 0.3)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    
+    ctx.beginPath();
+    ctx.fillStyle = ballColor;
+    ctx.strokeStyle = 'black';
+    ctx.arc(ballX, ballY, ballRadius, 0, 2*Math.PI);
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+    
+
+    if(ballX + ballRadius >= canvas.width || ballX - ballRadius <= 0 ) {
+        directionRight = !directionRight;
+    }
+
+    if(directionRight){
+        ballX = ballX + speedX;
+    }
+    else {
+        ballX = ballX - speedX;
+    }
+
+
+    if(ballY + ballRadius >= canvas.height || ballY - ballRadius <= 0) {
+        directionDown = !directionDown;
+    }
+    if(directionDown){
+        ballY = ballY + speedY;
+    }
+    else {
+        ballY = ballY - speedY;
+    }
+
+
+    window.requestAnimationFrame(animate);
+}
+
+animate();
+
+
+canvas.addEventListener("click", function(event){
+
+    var distX = Math.abs(ballX - event.clientX);
+    var distY = Math.abs(ballY - event.clientY);
+
+    if(distX < ballRadius && distY < ballRadius){
+        console.log('FIRE!!!!!!');
+    }
+    
+})

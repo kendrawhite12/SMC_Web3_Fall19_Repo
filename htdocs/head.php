77f8991bc5bdb5082b8e343862bd95713af0ff74
kendrawhite12 <47585497+kendrawhite12@users.noreply.github.com>
2019-12-03 13:17:09 -0800

    index.php head.php  header. php
    Dry run midterm

---
+++ htdocs/head.php
@@ -0,0 +1,16 @@
+(ADD BOOTSTRAP CSS AND JS LINKS)
+<style>
+
+</style>
+<body>
+    <header>
+        <nav>
+            <ul> 
+                <li><a href="index.php">Home</li>
+                <li><a href="about.php">About</li>
+                <li><a href="contact.php">Contact</li>
+                <li><a href="blog.php">Blog</li>
+            </ul>
+        </nav>
+    <header>
+
